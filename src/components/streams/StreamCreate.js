b8249bfaf12ca2e1c2ac3246e47c4b274da9f0e9
Sai Ram <732124@cognizant.com>
2020-02-06 11:24:57 +0530

    create stream

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,8 +1,67 @@
 import React from 'react';
+import {Field, reduxForm} from 'redux-form';
 
+class StreamCreate extends React.Component {
 
-const StreamCreate = () => {
-    return <div> Stream Create </div>
+  renderError({error, touched}){
+   if(touched && error) {
+    return (
+      <div className ='ui error message'>
+        <div className ='header'> {error}</div>
+        </div>
+    );
+  }
+  }
+
+   renderInput = ({input, label, meta}) => {
+    const className = `field ${meta.error && meta.touched ? 'error' : ''}`
+     return (
+       <div className={className}>
+         <label>{label}</label>
+        <input {...input} autoComplete='off'/>
+        <div>{this.renderError(meta)}</div>
+      </div>
+     )
+  }
+
+  onSubmit(formValues){
+   console.log(formValues);
+  }
+
+    render(){
+      return (
+      <form  onSubmit= {this.props.handleSubmit(this.onSubmit)} className='ui form error'>
+         <Field 
+         name='title' 
+         component={this.renderInput} 
+         label='Enter Title'/>
+         <Field 
+         name='description' 
+         component={this.renderInput} 
+         label='Enter Description'/>
+
+         <button className='ui button primary'>Submit</button>
+      </form>
+      );
+    }
+    
 }
 
-export default StreamCreate;
+const validate = formValues => {
+  const errors = {};
+
+  if(!formValues.title){
+    errors.title = 'you must enter a title';
+  }
+   if(!formValues.description){
+    errors.description = 'you must enter a description';
+  }
+  return errors;
+}
+
+
+
+export default reduxForm({
+  form: 'stream-create',
+  validate
+})(StreamCreate);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 import authReducer from './authReducer';
+import { reducer as fromReducer } from 'redux-form'
 
 
 export default combineReducers({
-  auth: authReducer
+  auth: authReducer,
+  form: fromReducer
 });
